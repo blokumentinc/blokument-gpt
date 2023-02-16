aba2079f52ed5af17eb5b607a2270b09ace96d68
Saurabh Parmar <saurabh@blokumet.com>
2023-02-16 13:42:02 +0530

    enabled caching

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,9 @@
 FROM python:3.8-slim-buster
 
+ENV OPENAI_API_KEY ""
 ENV MODEL_ENGINE "text-davinci-002"
 
-
 COPY requirements.txt .
-
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
--- gpt3_script.py
+++ gpt3_script.py
@@ -1,25 +1,43 @@
 import openai
+import os
 from flask import Flask, request
 
-# Add your OpenAI API key
-openai.api_key = "API"
+# Set the OpenAI API key and engine from environment variables
+openai.api_key = os.environ['OPENAI_API_KEY']
+openai_engine = os.environ.get('OPENAI_ENGINE', 'text-davinci-002')
+
+# Create a pre-initialized OpenAI instance
+openai_instance = openai.api.OpenAI()
+
+# Create a dictionary to store cached responses
+response_cache = {}
 
 app = Flask(__name__)
 
-@app.route('/generate_text', methods=['POST'])
+@app.route('/generate_text')
 def generate_text():
-    prompt = request.json['prompt']
-    completions = openai.Completion.create(
-        engine="text-davinci-002",
+    prompt = request.args.get('prompt')
+    
+    # Check the cache for the response
+    if prompt in response_cache:
+        return response_cache[prompt]
+    
+    # If the response is not in the cache, make a new request to the OpenAI API
+    completions = openai_instance.Completion.create(
+        engine=openai_engine,
         prompt=prompt,
         max_tokens=1024,
         n=1,
         stop=None,
         temperature=0.7,
     )
-
-    message = completions.choices[0].text
-    return message.strip()
+    
+    message = completions.choices[0].text.strip()
+    
+    # Add the response to the cache
+    response_cache[prompt] = message
+    
+    return message
 
 if __name__ == '__main__':
     app.run(debug=True)

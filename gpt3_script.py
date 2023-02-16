dc8ba1a949809d436f23b93a2382cdb14ae8bd2c
Saurabh Parmar <saurabh@blokumet.com>
2023-02-16 14:08:26 +0530

    added env

--- gpt3_script.py
+++ gpt3_script.py
@@ -1,29 +1,19 @@
-import openai
 import os
+import openai
 from flask import Flask, request
 
-# Set the OpenAI API key and engine from environment variables
-openai.api_key = os.environ['OPENAI_API_KEY']
-openai_engine = os.environ.get('OPENAI_ENGINE', 'text-davinci-002')
+# Add your OpenAI API key
+openai.api_key = os.environ.get("OPENAI_API_KEY", "")
 
-# Create a pre-initialized OpenAI instance
-openai_instance = openai.api.OpenAI()
-
-# Create a dictionary to store cached responses
-response_cache = {}
+# Set the OpenAI engine name from the environment variable
+openai_engine = os.environ.get("OPENAI_ENGINE", "text-davinci-002")
 
 app = Flask(__name__)
 
-@app.route('/generate_text')
+@app.route('/generate_text', methods=['POST'])
 def generate_text():
-    prompt = request.args.get('prompt')
-    
-    # Check the cache for the response
-    if prompt in response_cache:
-        return response_cache[prompt]
-    
-    # If the response is not in the cache, make a new request to the OpenAI API
-    completions = openai_instance.Completion.create(
+    prompt = request.json['prompt']
+    completions = openai.Completion.create(
         engine=openai_engine,
         prompt=prompt,
         max_tokens=1024,
@@ -31,13 +21,9 @@ def generate_text():
         stop=None,
         temperature=0.7,
     )
-    
-    message = completions.choices[0].text.strip()
-    
-    # Add the response to the cache
-    response_cache[prompt] = message
-    
-    return message
+
+    message = completions.choices[0].text
+    return message.strip()
 
 if __name__ == '__main__':
     app.run(debug=True)
